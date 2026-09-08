51e171fdbd790c4fb438425ae306ea35b084db51
EasyFacilongo <adrianmoncada089@gmail.com>
2026-09-07 22:07:32 -0500

    Delete Habilities directory too

--- Habilities/spiderSense.js
+++
@@ -1,8 +0,0 @@
-// habilities/spiderSense.js -- habilidad de sentido arácnido: toggle (el tiempo se ralentiza
-// mientras este activo). Antes vivia inline en el case 'ControlLeft'/'ControlRight' del keydown
-// de spiderswing.html; la logica es identica, solo movida a funcion para que ese case pueda
-// llamarla igual que a cualquier otra habilidad. La tecla que la dispara sigue viniendo de
-// habilities/controls.json (campo "spiderSense"), no de aca.
-function toggleSpiderSense(){
-  spiderSenseOn = !spiderSenseOn;
-}
